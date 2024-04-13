8eaf8c308bc15971453eb0a7f535a52c609f92ac
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-04-13 16:59:04 -0400

    Add button clicks to the terminal.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
-import {Game} from './game';
+import { Game } from "./game";
 
-
+const rockBtn = document.querySelector("#rock-button") as HTMLButtonElement;
+const paperBtn = document.querySelector("#paper-button") as HTMLButtonElement;
+const scissorsBtn = document.querySelector(
+  "#scissors-button"
+) as HTMLButtonElement;
+if (rockBtn) {
+  rockBtn.addEventListener("click", () => {
+    console.log("rock");
+  });
+}
+if (paperBtn) {
+  paperBtn.addEventListener("click", () => {
+    console.log("paper");
+  });
+}
+if (scissorsBtn) {
+  scissorsBtn.addEventListener("click", () => {
+    console.log("scissors");
+  });
+}
 const game = new Game();
 game.displayOptions();
-console.log('\n');
-console.log(game.playRound( Math.floor(Math.random() * 3)));
+console.log("\n");
+console.log(game.playRound(Math.floor(Math.random() * 3)));
